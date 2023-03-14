f8515feef9bb6c90dc3e59df979bad56511859b3
vutt2809 <tranthanhvu2809@gmail.com>
2023-03-14 15:03:21 +0700

    init source

---
+++ .prettierrc.js
@@ -0,0 +1,16 @@
+module.exports = {
+    printWidth: 100,
+    tabWidth: 4,
+    useTabs: false,
+    semi: true,
+    singleQuote: true,
+    trailingComma: 'es5',
+    bracketSpacing: true,
+    jsxBracketSameLine: false,
+    arrowParens: 'always',
+    rangeStart: 0,
+    parser: 'babel',
+    requirePragma: false,
+    insertPragma: false,
+    proseWrap: 'preserve',
+};
